189352a98cb0f4af0e78b30f31ec82c2daf71ce6
DiegoDam8 <diego.parram3@gmail.com>
2024-03-25 19:25:52 +0100

    FEAT: calculation of cost per diner in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
-import "./style.css";
+//Coste de la cena
+const costeTotal = 120;
+// Coste de las bebidas
+const costeBebidas = 18;
+//Número de comensales
+const numeroComensales = 6;
 
-console.log("Hello Typescript!");
+//Cálculo de coste sin bebidas
+const costeSinBebidas = costeTotal - costeBebidas;
+//Cálculo de coste por comensales sin las bebidas
+const costePorComensal = costeSinBebidas / numeroComensales;
+
+//Resultado:
+console.log(`El costo por comensal es de ${costePorComensal}`);
